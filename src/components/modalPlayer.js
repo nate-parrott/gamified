8682f2e3e96af16b5a8fe17c700a983c590d6ea3
Nate Parrott <nateparro2t@gmail.com>
2018-09-09 22:32:01 -0400

    wip

--- src/components/modalPlayer.js
+++ src/components/modalPlayer.js
@@ -3,10 +3,11 @@ import './modalPlayer.css';
 import { uuid } from './utils';
 
 export class ModalItem {
-	constructor(render) {
-		// render: (full: bool) -> content
+	constructor(render, itemClass) {
+		// render: ({full: bool}) -> content
 		this.identifier = uuid();
 		this.render = render;
+		this.itemClass = itemClass;
 	}
 }
 
@@ -17,24 +18,17 @@ export class ModalPlaylist {
 	}
 }
 
-const ModalItemView = ({ classes, item, onBack, onForward, onDismiss }) => {
-	let className = (classes || []).join(' ') + ' ModalItemView';
+const ModalItemView = ({ item, onBack, onForward, onDismiss, offset }) => {
+	let className = `ModalItemView offset_${offset} ${item.itemClass || ''}`;
+	let content = item.render({ full: (offset === 0) });
 	return (
 		<div className={className}>
-			<div className='content'>{ item.render() }</div>
+			<div className='content'>{ content }</div>
 			{onBack ? <div className='control back' onClick={onBack} key='back' /> : null}
 			{onForward ? <div className='control forward' onClick={onForward} key='forward' /> : null}		
 			<div className='control dismiss' onClick={onDismiss}>Dismiss</div>
 		</div>
 	)
-}
-
-
-let classForIndex = (i) => {
-	if (i > 3) {
-		return 'index-toofar';
-	}
-	return `index-${i}`;
 }
 
 export default class ModalPlayer extends React.Component {
@@ -51,16 +45,33 @@ export default class ModalPlayer extends React.Component {
 	}
 	render() {
 		let {playlist} = this.props;
+		
 		let {itemIndex} = this.state;
 		let items = playlist ? playlist.items : [];
-		let currentItem = itemIndex < items.length ? items[itemIndex] : null;
-		let itemView = currentItem ? <ModalItemView className='ModalPlayer' item={currentItem} onBack={this.back.bind(this)} onForward={this.forward.bind(this)} onDismiss={this.dismiss.bind(this)} /> : null;
-		let onPlayerClick = (e) => {
+		
+		let offsets = [0, 1, 2];
+		let itemViews = offsets.map((offset) => {
+			let idx = itemIndex + offset;
+			if (idx < 0 || idx >= items.length) {
+				return null;
+			}
+			let actionProps = {};
+			if (offset === 0) {
+				actionProps = {
+					onBack: this.back.bind(this),
+					onForward: this.forward.bind(this),
+					onDismiss: this.dismiss.bind(this)
+				};
+			}
+			return <ModalItemView key={idx} className='ModalPlayer' item={items[idx]} offset={offset} {...actionProps} />;
+		});
+		
+		let dismiss = (e) => {
 			if (e.currentTarget === e.target) {
 				this.dismiss();
 			}
 		}
-		return <div className='ModalPlayer' onClick={onPlayerClick}>{itemView}</div>;
+		return <div className='ModalPlayer' onClick={dismiss}>{itemViews}</div>;
 	}
 	back() {
 		this.advance(-1);
