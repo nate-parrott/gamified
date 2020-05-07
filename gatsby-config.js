d6eada312ee5c7e787070e0557acacb49578e688
Nate Parrott <nate@thebrowser.company>
2020-05-07 13:49:00 -0400

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: 'Nate Parrott’s Nice and Engaging Website',
   },
-  plugins: ['gatsby-plugin-react-helmet']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-37604830-9",
+      },
+    },
+  ],
 }
